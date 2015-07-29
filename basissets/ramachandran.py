7a2f9c322993c5db64abfcaaf526d11d52646882
Francesca Vitalini <vitalini@mi.fu-berlin.de>
2015-07-29 20:25:09 +0200

    debugged ramachandran.py

--- basissets/ramachandran.py
+++ basissets/ramachandran.py
@@ -4,7 +4,7 @@ import itertools as it
 
 class RamachandranBasis(BasisSet):
 
-    def __init__(self, restype, order=2, ff='AMBER99SB_ILDN'):
+    def __init__(self, restype, order=2, ff='ff_AMBER99SB_ILDN', radiants=True):
         """Basis set on the phi/psi torsion space of a single amino acid.
 
             Parameters
@@ -14,9 +14,11 @@ class RamachandranBasis(BasisSet):
             order : int, default=2
                 order of basis function family. Here equal to the number of nontrivial MSM eigenvectors on phi/psi space
                 to be used. The output will have order+1 columns
-            ff : str, default='AMBER99SB_ILDN'
-                Force field to be used. Options: 'AMBER99SB_ILDN' | 'AMBER03' | 'CHARMM27' | 'OPLSAA' | 'GROMOS43a1'
+            ff : str, default='ff_AMBER99SB_ILDN'
+                Force field to be used. Options: 'ff_AMBER99SB_ILDN' | 'ff_AMBER03' | 'ff_CHARMM27' | 'ff_OPLSAA' | 'ff_GROMOS43a1'
                  | ...
+	    radiants : True
+		expected inputs as radiants. If false, treats input as degrees
 
             References
             ---------
@@ -26,6 +28,7 @@ class RamachandranBasis(BasisSet):
         """
         self.order = order
         self.ff = ff
+	self.radiants = radiants
         self.RBV = np.load('ResiduesEigenvectors/Ac_'+restype+'_NHMe.npz')
         self.RBV = self.RBV[ff][:,:order+1]
        
@@ -35,7 +38,7 @@ class RamachandranBasis(BasisSet):
         ---------
         X : ndarray(T, 2)
             phi / psi angles for one residue in a  trajectory of length T
-	    The array is angles in degrees between 0 and 360
+	    The array is angles in radiants between 0 and 360
 
         Returns
         -------
@@ -45,16 +48,26 @@ class RamachandranBasis(BasisSet):
 
         """
 	binwidth=10 #binwidth
+        nbins=360/binwidth # number of bins per coordinate
+	if self.radiants==True:
+		X=X*(180/np.pi) # convert in radiants
+        if np.max(X)<360:
+                X=X+180
+        X[np.nonzero(X[:,0]>=360),0]=0
+        X[np.nonzero(X[:,1]>=360),1]=0
+        X[np.nonzero(X[:,0]<0),0]=0
+        X[np.nonzero(X[:,1]<0),1]=0
 	i=np.floor(X[:,0]/binwidth) #binned phi angle
 	j=np.floor(X[:,1]/binwidth) #binned psi angle
 	n=i*(nbins)+j #n_microstates traj
+	n=np.array(n, dtype=int)
         ev_traj=self.RBV[n,:] #project n_microstate trajectory of the monomer onto the eigenvector as ev_traj[t]=RBV[n[t]]
        	return ev_traj
 
 
 class RamachandranProductBasis(BasisSet):
 
-    def __init__(self, sequence, include_res=None, max_order=2, n_excite=2, ff='AMBER99SB_ILDN'):
+    def __init__(self, sequence, include_res=None, max_order=2, n_excite=2, ff='ff_AMBER99SB_ILDN', radiants=True):
         """Basis set as vector product of the residue centered basis functions evaluated on the phi/psi torsion space of the single amino acids.
 
         Parameters
@@ -71,8 +84,12 @@ class RamachandranProductBasis(BasisSet):
             number of Ramachandran basis functions allowed to be excited. The default value (2) will allow only single and double excitations as suggested in [5].
             Careful, all possible combinations (n_excite=len(sequence)) can be unmanageable. The minimal excitation is 1, where
             the excited states up to max_order are used for each amino acid in the chain.
-        ff : str, default='AMBER99SB_ILDN'
-	    Force field to be used. Options: 'AMBER99SB_ILDN' | 'AMBER03' | 'CHARMM27' | 'OPLSAA' | 'GROMOS43a1'
+        ff : str, default='ff_AMBER99SB_ILDN'
+	    Force field to be used. Options: 'ff_AMBER99SB_ILDN' | 'ff_AMBER03' | 'ff_CHARMM27' | 'ff_OPLSAA' | 'ff_GROMOS43a1'
+
+	radiants : True
+                expected inputs as radiants. If false, treats input as degrees
+
 
         References
         ---------
@@ -86,6 +103,10 @@ class RamachandranProductBasis(BasisSet):
         self.max_order = max_order
         self.n_excite = n_excite
         self.ff = ff
+	self.radiants = radiants
+	
+	if self.include_res == None:
+		self.include_res=np.ones(len(self.sequence), dtype=bool)
 
 #	ind = np.arange(len(self.sequence), dtype=int) #array of indices
 #	self.sequence = [self.sequence[i] for i in ind[[self.include_res]] # new sequence including only the residues labled as true
@@ -94,18 +115,19 @@ class RamachandranProductBasis(BasisSet):
 	self.nres = len(self.sequence) # number of residues to include
 	combinations = it.combinations(np.arange(self.nres), self.n_excite) # select residues to excite
 	basis_set_list = [] # initialize list of basis functions
-	basis_set_list.append(np.zeros(self.nres)) # add the basis function where none of the residues is excited
 	for c in combinations: # loop over combinations
 		current_basis = np.zeros(self.nres, dtype=int) # initialize basis array
-		excitation_res = it.combinations_with_replacement(np.arange(self.max_order+1), self.n_excite) # all possible combinations of eigenvectors in the excited states
-		counter=0 # set to avoid adding the 00000 basis function all the times
+		excitation_res = it.product(np.arange(self.max_order+1), repeat=self.n_excite)# all possible combinations of eigenvectors in the excited states
 		for p in excitation_res: # loop over the combinations of eigenvectors
 			current_basis[np.array(c)]=np.array(p) # select spots to excite
-			if counter > 0:
-				basis_set_list.append(current_basis) # add excitations to list
-			counter = counter + 1
-	self.num_prod=len(basis_set_list) # number of products
-	self.basis_set_list=basis_set_list # make list of basis functions available out of the function
+			basis_set_list.append(np.copy(current_basis)) # add excitations to list
+	basis_set_list=np.array(basis_set_list, dtype=int)
+	#find unique elements in basis set list
+	b = np.ascontiguousarray(basis_set_list).view(np.dtype((np.void, basis_set_list.dtype.itemsize * basis_set_list.shape[1])))
+	_, idx =np.unique(b, return_index=True)
+	unique_basis=basis_set_list[idx]
+	self.num_prod=len(unique_basis) # number of products
+	self.basis_set_list=np.array(unique_basis, dtype=int) # make list of basis functions available out of the function
 
     def map(self, X):
         """
@@ -116,20 +138,25 @@ class RamachandranProductBasis(BasisSet):
 
         Returns
         -------
-        Y : ndarray(T, n)
-            The basis set values, evaluated for every trajectory time point.
-            Each of the n elements represents a product of the residue basis
-            sets.
+        Prod : ndarray(T, n)
+             The basis set values, evaluated for every trajectory time point.
+             Each of the n elements represents a product of the residue basis
+             sets.
+
+	basis_set_list : nparray(nprod, nres)
+			 list of basis functions as numpy array with nprod = nuber of basis functions and nres = number of residues
 
         """
+
 	X=X[:,np.repeat(self.include_res, 2)] # select only residues labled as true
+	print X[0:10,:]+180
 	Prod=np.ones((X.shape[0],self.num_prod)) # initialize products matrix
 	q=0 # counter for the residue
 	for r in range(self.nres): # loop over residues
-		RBV=RamachandranBasis(self.sequence[r], order=self.max_order, ff=self.ff) # load eigenvectord for the residue
+		RBV=RamachandranBasis(self.sequence[r], order=self.max_order, ff=self.ff, radiants=self.radiants) # load eigenvectord for the residue
 		RBV_traj=RBV.map(X[:,q:q+2]) # project trajectory over eigenvectors
 		Prod=Prod*RBV_traj[:,self.basis_set_list[:,r]] # vectorizing over all possible states of excitations of residue r and multiplying the projected trajectory to the full product
-		q =+ 2
+		q = q + 2
 
-	return Prod
+	return Prod, self.basis_set_list
         
